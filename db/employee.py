581dab4e1f89dbd2c6a42adf01c68da0a877b15e
florizdesigner <vyacheslav.sokolovv@icloud.com>
2024-05-12 21:23:42 +0300

    temp commit | refactor code, added typed successful response

--- db/employee.py
+++ db/employee.py
@@ -13,10 +13,10 @@ class EmployeeController:
             db_connection = __db_connect__()
             cursor = db_connection.cursor()
 
-            logging.info(msg=f"Started search all employees")
-            cursor.execute("SELECT * FROM employees")
-            result = cursor.fetchall()
-            logging.info(msg=f"Success! Result: {result}")
+            employees = cursor.execute("SELECT * FROM employees").fetchall()
+            response = DBResponse(status="success", message=None, employee=employees).to_dict()
+
+            return response
         except Exception as e:
             logging.error(msg=e)
         finally:
@@ -29,10 +29,10 @@ class EmployeeController:
             cursor = db_connection.cursor()
             logging.info(msg=f"Started search employee by id: {id}")
 
-            cursor.execute(f"SELECT * FROM employees WHERE id = {id}")
-            result = cursor.fetchone()
-            logging.info(msg=f"Employee: {result}")
-            return result
+            employee = cursor.execute(f"SELECT * FROM employees WHERE id = {id}").fetchone()
+            response = DBResponse(status="success", message=None, employee=employee).to_dict()
+
+            return response
         except Exception as e:
             logging.error(msg=e)
         finally:
@@ -50,7 +50,11 @@ class EmployeeController:
             cursor.execute(f"INSERT INTO employees (first_name, last_name, telegram_id, telegram_username, is_fulltime, work_group, type_of_fulltime_shifts, is_absence) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
                            (user["first_name"], user['last_name'], user['telegram_id'], user['telegram_username'], user['is_fulltime'], user['work_group'], user['type_of_fulltime_shifts'], user['is_absence']))
             db_connection.commit()
-            logging.info(msg="User successfully added!")
+
+            added_user = cursor.execute(f"SELECT * FROM employees WHERE telegram_id = {user.telegram_id}").fetchone()
+            response = DBResponse(status="success", message="User successfully added", employee=added_user).to_dict()
+
+            return response
         except Exception as e:
             db_connection.rollback()
             logging.error(msg=e)
@@ -79,17 +83,15 @@ class EmployeeController:
                 else:
                     employee_str += str(ele)
 
-            print(f"update employees set ({employee_str}) = {tuple(employee.values())} where id = {id}")
-
             cursor.execute(f"update employees set ({employee_str}) = {tuple(employee.values())} where id = {id}")
 
             db_connection.commit()
 
             cursor.execute(f"select * from employees where id = {id}")
             updated_employee = Employee(*cursor.fetchone()[1:]).to_dict()
+            response = DBResponse(status="success", message="User successfull edited", employee=updated_employee).to_dict()
 
-            return DBResponse(status="success", message="User successfull edited", employee=updated_employee)
-
+            return response
         except Exception as e:
             db_connection.rollback()
             logging.error(msg=e)
@@ -106,9 +108,7 @@ class EmployeeController:
 
             cursor.execute(f"DELETE FROM employees WHERE id = {id}")
             db_connection.commit()
-
-            response = DBResponse(status="success", message=f"User id: {id} is successfully deleted from DB").to_dict()
-            logging.info(msg=response)
+            response = DBResponse(status="success", message=f"User id: {id} is successfully deleted from DB", employee=None).to_dict()
 
             return response
         except Exception as e:
